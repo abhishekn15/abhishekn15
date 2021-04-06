1e8e8a4a366ddfdc5e7d0177e7e41a7eb898fa3d
hp <hp@LAPTOP-CESU4ELL>
2021-04-06 14:11:33 +0530

    new code for String length

---
+++ git mergin project/src/india/Learn.java
@@ -0,0 +1,18 @@
+package india;
+
+public class Learn {
+
+	public static void main(String[] args) {
+		String name="abhishek singh";
+		int length=0;
+	char c[]=name.toCharArray();
+	for(  char cc: c) {
+		
+		length++;
+		
+	}
+	System.out.println(length);
+
+	}
+
+}
